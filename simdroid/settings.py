cfa2f9ca06694a29a355081fa99a62c81b13ba6e
Ahadjon Ubaydullayev <ahadjon.ubaydullayev16@gmail.com>
2022-12-14 15:43:13 -0400

    requirements modified

--- simdroid/settings.py
+++ simdroid/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,8 +127,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
